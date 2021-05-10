3157faa56e051e2cd813fea5aa96d4927b031cc5
soup-bowl <code@soupbowl.io>
2021-05-10 19:48:49 +0100

    Gui Improvements.

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import PySimpleGUI as sg
 
 welcomeLabel = "Choose an Operating System to begin."
+bgColor      = 'black'
+
+colApple = sg.Column([ [sg.Button('', key='sys9', image_filename='./logo-apple.png')], [sg.Text('System 7')] ], element_justification='center')
+colWin   = sg.Column([ [sg.Button('', key='dos', image_filename='./logo-windows.png')], [sg.Text('DOS')] ], element_justification='center')
 
 layout = [
-	[sg.Text(welcomeLabel, justification='center', size=(200,1))],
-	[
-		sg.Button('', key='sys9', image_filename='./logo-apple.png'),
-		sg.Button('', key='dos', image_filename='./logo-windows.png')
-	],
+	[sg.Image('./Happy_Mac.gif')],
+	[sg.Text(welcomeLabel, size=(200,1))],
+	[colApple, colWin],
 	[sg.Cancel('Exit', key='quit', auto_size_button=True)]
 ]
 
-window = sg.Window(welcomeLabel, layout, element_justification='c', resizable=True, auto_size_buttons=True).finalize()
+window = sg.Window(
+	welcomeLabel,
+	layout,
+	element_justification='c',
+	text_justification='c',
+	resizable=True,
+	auto_size_buttons=True	
+).finalize()
+
 window.maximize()
 
 while True:             
