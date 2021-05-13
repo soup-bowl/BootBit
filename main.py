697f7a87b64e37a8b3aa7904a40136da56c0a9f7
soup-bowl <code@soupbowl.io>
2021-05-13 17:36:00 +0100

    Removing PySimpleGUI for Tkinter.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import PySimpleGUI as sg
+import tkinter as tk
+from PIL import Image, ImageTk
+from functools import partial
 import subprocess
 import json
 import sys
@@ -6,68 +8,67 @@ import sys
 icon    = 'R0lGODlhfACAAPMKACIiIlVVVYiIiN0AAAC7AERERHd3dwAAAMzM/8zMzP///wAAAAAAAAAAAAAAAAAAACH5BAEAAAoALAAAAAB8AIAAAAT+UMlJqzo46827/2AojodlnqhErmzrimkcv3Rtg3Ju3nxP6zpNYkgsGo/IpHLJbDoTGiBK+Kxar1hmVLrLZL/g8HLLrVDF6DSWXJ6c1fB4kt2+eOX4PDRTp7z1gGJ0bX+Bhmt8fSp3SwWOj5CRkpOUlZaXjk2DZYVHmJ+goaCaiYqdRqKpqqmkGIqLGE2rs7SUrSWvp5AIvL2+v8DBwsPExbwGyAZzpX26j8bQ0dLRycpIm1zOjtPc3dzVy665jKjP3ufoweDXzHXaBenx8etH2FLv8vne9Eb2QPj6AlJLFg6XKXJFdglcSIxfEX9BEBJRyLDiL4dEIOYAaLEjAoz+QzTK4OixIsg94g7GQkKxJMOTImdIHNLSmAabGXBimAazHaGZCWoWuzk0Z9Gd0nqmbAZUKDGiT41GRToQWcFXdlZ6MhcN6jCvwsAWU2qQqdZy26SJBbb2V1thZLGS1HmArt2kBNktddeUK7S3vQDzEnwxbz2fnPqm7Sr1a+Owj8ca7oc4m2J4FQmfizvubEK/AjXvm/yw8r3LFkV346zyQBKn+VR/I53R9D/UmSPrY23WNUvQAWXzpB3SdkTPE4HrE47Xqt6yfJHTVB5bdz7e0X1vXVydg0XsP6UHpX7Ow3fiKKGH144W8/IO550f3rv+NXlv5k2ij5lirsuA4CX+Jh5s/82zn3Eb4VagQAFaNuB9C44mH2X0CcjeZ9xNIxtzwDR42oMZqmUdZFSh4+FtILrHzYYjNmfNfOpZaF+IjJV4VzonHndhcjSuuAKDB1bo4I7T9ajhjwAGGeOQM6pYHpK7KSmXguiwAOSEpQn5IZHjGRmhhC9SuOSWTX6ZJJa1aYkilwSaORuaxampY5luypNjginWaaeUnbEJoZ7G3DkSlYCuxmdrdBYK5lV9NlHNo5BGKumklFYq6S1TileEpZx26mmnmDbKxKeklkpqqIiOauqqrEaKam+HxJoFf1MAJeutz2XKJa68pjnmmr0Gm6uowhabnq7GGkvrCaf+JBvrsl1o6qwh0FrQ7LSBVGuGrdgCoq0f3Habx7duhCuuHOTCsuu5eKSb1brsxuHutfHCMa+59aJxr3gE9Otvv3r4wMGr2SXx778BC0zvscQacbC/CSssbZy/zonEwwCPK/HEDKd6xAAghwyyHq1SSnB9SIgsMsklX6oFgoOKp3LILLcM6ckypjzzADXbfFKWFeMJb776wiwTx0SHse/QSYOxdNMRBx0z01AjIqfQVbdrdH/4Zm3F017bu3WtSIf9BNhmF3311GmngXbbTo899cZ0C4wVs3Xn7cPd0ert9w98b/v34C0ELvgBASSu+OKMN674LABELvnklFdu+eWsHhgOLgaOd+455JeHLjrmHWheLueep7446KO3Pnrmpr+r+uwBsO767ZXDbroGPkPauSi4h6675rz3Xs3voQRPOgexv2v88Y4Dr3zupcde/PMGIA/K9NQzb30GAoQv/vjkl2/++JZq/0nrwxuuwfnwx09++tEn/3r1u4Mv//7n09+49KJrX+Dex78Coq9S6sME+/BHPMI5kATNe6AEQxDBCVrQe/m7oAalNoEIAAA7'
 font    = 'Arial 16'
 welcome = 'Choose an Operating System to begin.'
-sg.theme('SystemDefault1')
 
-data = None
+window = tk.Tk()
+window.attributes('-fullscreen', True)
+
+data  = None
+icons = {}
+funct = {}
 try:
 	with open("./config.json") as json_file:
 		data = json.load(json_file)
 except FileNotFoundError:
 	msg = 'No configuration file found. A config.json file is required.'
-	sg.Popup(msg, title='config.json missing', keep_on_top=True)
 	print(msg)
 	sys.exit(1)
 
-columns = []
+def shutdown_sys():
+	subprocess.Popen(['sudo', 'shutdown', '-r', 'now'])
+	exit()
+
+def quit_app():
+	exit()
+
+def load_app(command, cwd=None):
+	try:
+		op_path = None if not cwd else cwd
+
+		print("Executing '"+ command + ("." if op_path == None else "' (in directory '" + op_path + "').") )
+		process = subprocess.Popen(command.split(), cwd=op_path)
+	except IndexError:
+		#sg.Popup('No command specified.', title='No runner specified', keep_on_top=True)
+		print("Failed, no command found.")
+	except FileNotFoundError:
+		#sg.Popup('Couldn\'t find the requested application.', title='No application', keep_on_top=True)
+		print("Failed, no application found.")
+
+imgLogo = tk.PhotoImage(data=icon)
+frmLogo = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
+frmLogo.grid(row=0, column=0, columnspan=2)
+lblLogo = tk.Label(master=frmLogo, image=imgLogo)
+lblLogo.pack()
+
+frmWelcome = tk.Frame(master=window)
+frmWelcome.grid(row=1, column=0, columnspan=2)
+lblWelcome = tk.Label(master=frmWelcome, text=welcome, font=font)
+lblWelcome.pack()
+
 for i, entry in enumerate(data['options']):
-	columns.append(
-		sg.Column([
-			[sg.Button(key=entry['name'], image_filename=entry['logo'], size=(5,5))],
-			[sg.Text(entry['name'])]
-		], element_justification='c')
-	)
+	icons[i] = ImageTk.PhotoImage(file=entry['logo'])
 
-layout = [
-	[sg.Image(data=icon)],
-	[sg.Text(welcome)],
-	columns,
-	[sg.Cancel('Exit', key='quit'), sg.Button(button_text='Shutdown', key='shut')]
-]
+	frmItem = tk.Frame(master=window)
+	frmItem.grid(row=2, column=i)
+	btnItem = tk.Button(master=frmItem, image=icons[i], command=partial( load_app, entry['command'], entry['cwd'] ))
+	btnItem.pack()
 
-window = sg.Window(
-	welcome,
-	layout=layout,
-	element_justification='c',
-	text_justification='c',
-	resizable=True,
-	auto_size_buttons=True,
-	font=font
-).finalize()
+frmClose = tk.Frame(master=window)
+frmClose.grid(row=3, column=0)
+btnClose = tk.Button(master=frmClose, text='Close', command=quit_app, font=font)
+btnClose.pack()
+frmShutdown = tk.Frame(master=window)
+frmShutdown.grid(row=3, column=1)
+btnShutdown = tk.Button(master=frmShutdown, text='Shutdown', command=shutdown_sys, font=font)
+btnShutdown.pack()
 
-window.maximize()
-
-while True:
-	event, values = window.read()
-
-	if event == 'shut':
-		process = subprocess.Popen(['sudo', 'shutdown', '-r', 'now'])
-		break;
-
-	if event in (sg.WIN_CLOSED, 'quit'):
-		break
-	
-	for entry in data['options']:
-		if event == entry['name']:
-			try:
-				op_path = None if not entry['cwd'] else entry['cwd']
-
-				print("Executing '"+ entry['command'] + ("." if op_path == None else "' (in directory '" + op_path + "').") )
-				process = subprocess.Popen(entry['command'].split(), cwd=op_path)
-			except IndexError:
-				sg.Popup('No command specified.', title='No runner specified', keep_on_top=True)
-				print("Failed, no command found.")
-			except FileNotFoundError:
-				sg.Popup('Couldn\'t find the requested application.', title='No application', keep_on_top=True)
-				print("Failed, no application found.")
-
-window.close()
+window.mainloop()
